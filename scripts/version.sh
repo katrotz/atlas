df076f1308b43ac272e8429ba16ee344e44fff03
Vyacheslav Cotruta <veaceslav.cotruta@webtrekk.com>
2019-08-04 22:14:16 +0200

    Updates npm version mechanism

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,5 +2,5 @@
 
 VERSION=$1
 
-PATH=$(npm bin):$PATH lerna run version -- ${VERSION}
+PATH=$(npm bin):$PATH lerna exec -- npm version ${VERSION}
 #PATH=$(npm bin):$PATH lerna version patch --yes --amend --include-merged-tags --no-changelog --tag-version-prefix='v'
