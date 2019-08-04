f305179593646f866e9a58435fb6d9f197969891
Vyacheslav Cotruta <veaceslav.cotruta@webtrekk.com>
2019-08-04 18:05:16 +0200

    Adds deployment infrastructure

---
+++ scripts/publish.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PATH=$(npm bin):$PATH lerna version patch --yes --amend --include-merged-tags --no-changelog --no-push --tag-version-prefix='v'
+
+PATH=$(npm bin):$PATH lerna publish from-git --yes --contents dist --dist-tag latest --pre-dist-tag next
