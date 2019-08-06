1adfc4c80241f60200f43b72b5a9bcf91c9d17f0
Vyacheslav Cotruta <veaceslav.cotruta@webtrekk.com>
2019-08-06 22:10:55 +0200

    Prepares for npm publish

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-VERSION=$1
-
-PATH=$(npm bin):$PATH lerna exec -- rm -rf dist
-PATH=$(npm bin):$PATH lerna exec -- npm version ${VERSION}
+if [[ "$1" = "--pre" ]]
+then
+    npm test
+elif [[ "$1" = "--post" ]]
+then
+    git push --tags
+else
+    VERSION=$1
+    PATH=$(npm bin):$PATH lerna exec -- rm -rf dist
+    PATH=$(npm bin):$PATH lerna exec -- npm version ${VERSION}
+fi
