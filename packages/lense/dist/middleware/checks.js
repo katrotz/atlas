cea41a70b3ed884a5c267ed00ae8cc84e3c4817c
Vyacheslav Cotruta <veaceslav.cotruta@webtrekk.com>
2019-08-06 22:18:20 +0200

    Reset

--- packages/lense/dist/middleware/checks.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genericCheck = (req, res, next) => {
-    next();
-};
-//# sourceMappingURL=checks.js.map
--- packages/lense/dist/middleware/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("./common");
-const docs_1 = require("./docs");
-exports.default = [common_1.handleCors, common_1.handleBodyRequestParsing, common_1.handleCompression, docs_1.handleAPIDocs];
-//# sourceMappingURL=index.js.map
