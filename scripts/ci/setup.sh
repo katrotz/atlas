e670e3cfffcfa6191f7ed834717dff8f1d51917b
Vyacheslav Cotruta <veaceslav.cotruta@webtrekk.com>
2019-08-25 23:14:58 +0200

    Adjusts the publish mechanism

--- scripts/ci/setup.sh
+++ scripts/ci/setup.sh
@@ -9,26 +9,28 @@ GITHUB_REPO="atlas";
 NPM_REGISTRY="registry.npmjs.org";
 
 if [[ -n ${GITHUB_TOKEN} ]]; then
-    git remote set-url origin https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git
-    git config --global user.name "CI Server"
-    git config --global user.email "travis@example.com"
+    git remote set-url origin https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git;
+    git config --global user.name "CI Server";
+    git config --global user.email "travis@example.com";
 else
     echo "Expected the environment variable \$GITHUB_TOKEN to be set" && exit 1;
 fi
 
 if [[ -n ${NPM_TOKEN} ]]; then
-    touch ~/.npmrc
-    echo "//${NPM_REGISTRY}/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+    touch ~/.npmrc;
+    echo "//${NPM_REGISTRY}/:_authToken=${NPM_TOKEN}" >> ~/.npmrc;
 else
     echo "Expected the environment variable \$NPM_TOKEN to be set" && exit 1;
 fi
 
 if [[ -n "${TRAVIS_BRANCH}" ]]; then
-    CI_BRANCH_NAME="${TRAVIS_BRANCH}"
+    CI_BRANCH_NAME="${TRAVIS_BRANCH}";
 fi;
 
 if [[ -n "${TRAVIS_PULL_REQUEST_BRANCH}" ]]; then
-    CI_BRANCH_NAME="${TRAVIS_PULL_REQUEST_BRANCH}"
+    CI_BRANCH_NAME="${TRAVIS_PULL_REQUEST_BRANCH}";
 fi;
 
-export PUBLISH_BRANCH_NAME="${CI_BRANCH_NAME}"
+export PUBLISH_BRANCH_NAME="${CI_BRANCH_NAME}";
+
+echo "Running on branch ${PUBLISH_BRANCH_NAME}";
