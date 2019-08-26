5ae16acff76c6a30ba33c3b8fa53e5d53b5284e6
CI Server <travis@example.com>
2019-08-26 08:01:58 +0000

    chore(release): version 0.1.6

---
+++ packages/nebula/dist/middleware/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("./common");
+const docs_1 = require("./docs");
+exports.default = [common_1.handleCors, common_1.handleBodyRequestParsing, common_1.handleCompression, docs_1.handleAPIDocs];
+//# sourceMappingURL=index.js.map
